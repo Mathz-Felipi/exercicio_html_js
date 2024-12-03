43e2640d4415d56cfe7484946c182dac3ab65e67
Matheus Felipi <ma.felipi01@gmail.com>
2024-12-03 19:52:46 -0300

    Resumo das melhorias: Corrigir mensagens dinâmicas de sucesso e erro. Melhorar a exibição e a transição das mensagens de erro e sucesso com CSS. Substituir alert() por mensagens dinâmicas visíveis no próprio layout. Simplificar o código JavaScript, removendo variáveis desnecessárias e melhorando a legibilidade. Adicionar foco nos campos com erro, se necessário. Essas melhorias devem tornar o seu código mais eficiente, robusto e com uma experiência de usuário mais fluida.

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const form = document.getElementById('formulario');
 let formEValido = false;
 
 const botaoReset = document.getElementById('reset');
-botaoReset.addEventListener('click', function(){
-    location.reload()
-})
+botaoReset.addEventListener('click', function() {
+    document.getElementById('campoA').value = '';
+    document.getElementById('campoB').value = '';
+    document.querySelector('.resultadoCerto').classList.remove('show');
+    document.querySelector('.resultadoErro').classList.remove('show');
+});
 
     const numA = document.getElementById('campoA');
     const numB = document.getElementById('campoB');
@@ -16,22 +19,24 @@ botaoReset.addEventListener('click', function(){
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    const sucesso = `Muito bem, o valor do campo B: <b>${campoB.value}</b> é maior que o valor do campo A: <b>1${campoA.value}</b>.`
-    const erro = `Refaçao processo, o valor do campo B: ${campoB.value} não é maior que o valor do campo A: ${campoA.value}.`
+    const sucesso = `Muito bem, o valor do campo B: <b>${campoB.value}</b> é maior que o valor do campo A: <b>${campoA.value}</b>.`
+    const erro = `Refaçao processo, o valor do campo B: <b>${campoB.value}</b> não é maior que o valor do campo A: <b>${campoA.value}</b>.`
 
     formEValido = comparacao(numA.value,numB.value)
 
     if (formEValido){
         const containerMensagemSucesso = document.querySelector('.resultadoCerto');
         containerMensagemSucesso.innerHTML = sucesso;
-        containerMensagemSucesso.style.display = 'block';
+        containerMensagemSucesso.classList.add('show');
 
         numA.value = ''
         numB.value = ''
     }
 
     else {
-        alert(erro)
+        const containerMensagemErro = document.querySelector('.resultadoErro');
+    containerMensagemErro.innerHTML = erro;
+    containerMensagemErro.classList.add('show');
         
     }
 })
